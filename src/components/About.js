4907aefa0320904c564e5cfdcf6399810e435f9b
mrsteveson <patrick.steveson727@gmail.com>
2019-12-04 18:15:11 -0600

    project screenshots

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,12 @@ const About = () => {
     return(
         <div>
             <p>What I am about!</p>
+            <p>Hello, I am Patrick Steveson, a Full-Stack Developer. 
+                I have always been enthusiastic about technology and its limitless capabilities. 
+                I enjoy problem solving and improving my skills.
+            </p>
             <p>Front-End Back-End Python</p>
-            <p>Skills:</p>
+            <p>Skills</p>
             <p>HTML, CSS, Javascript, React, Node.js, Express, SQLite, PostgreSQL, Python, Django, Git</p>
         </div>
     );
